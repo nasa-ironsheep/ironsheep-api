3ca9c1b53af9e6615695f3c26bce24d528d625e2
orggue <jorgechato1@gmail.com>
2016-04-24 12:40:17 +0200

    python service

--- image.py
+++ image.py
@@ -1,77 +1,70 @@
-import sys
 import requests
 import json
+import mimerender
 
 from PIL import Image
 from StringIO import StringIO
 from datetime import date
+from flask import Flask
 
+mimerender = mimerender.FlaskMimeRender()
 
-class Grass:
-    api_url = "https://api.nasa.gov/planetary/earth/imagery"
-    lon = 100.75
-    lat = 1.5
-    cloud = True
+render_json = lambda **args: json.dumps(args)
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+app = Flask(__name__)
 
-        with open("config.json") as json_file:
-                json_data = json.load(json_file)
-                for key, value in json_data.items():
-                    if key == "api_key":
-                        self.api_key = value
+today = date.today()
+api_url = "https://api.nasa.gov/planetary/earth/imagery"
+lon = 100.75
+lat = 1.5
+cloud = True
+api_key = "JNkasX49VZ8ygFComU3nBu97zhj4EBV2c0AEBnsV"
+params = {}
+url = ""
+cloud_score = 0.1
+date = ""
 
-        self.today = date.today()
-        params = {
-                "lon": self.lon,
-                "lat": self.lat,
-                "date": self.today,
-                "cloud_score": self.cloud,
-                "api_key": self.api_key
-                }
-        self.api(params)
+@app.route('/')
+@app.route('/<float:lon>/<float:lat>')
+@mimerender(
+    default = 'json',
+    json = render_json
+)
+def greet(lon, lat):
+    req = requests.get(api_url, params={
+            "lon": lon,
+            "lat": lat,
+            "date": today,
+            "cloud_score": cloud,
+            "api_key": api_key
+            })
+    data = json.loads(req.content)
+    for key, value in data.items():
+        if key == "url":
+            url = value
+        if key == "cloud_score":
+            cloud_score = value
+        if key == "date":
+            date = value
+    # return {'c':url}
+    req2 = requests.get(url)
+    img = Image.open(StringIO(req2.content))
 
-    def api(self, params):
-        req = requests.get(self.api_url, params=params)
-        data = json.loads(req.content)
-        for key, value in data.items():
-            if key == "url" or key == "cloud_score" or key == "date":
-                setattr(self, key, value)
+    pix = img.load()
+    green = 0
+    for i in range(0, img.width):
+        for j in range(0, img.height):
+            if pix[i, j][0] < pix[i, j][1] and pix[i, j][2] < pix[i, j][1]:
+                green += 1
 
-    def create_image(self):
-        req = requests.get(self.url)
-        img = Image.open(StringIO(req.content))
-        return img
+    percent = float(green)/(img.width*img.height)
 
-    def grass(self):
-        img = self.create_image()
-        pix = img.load()
-        green = 0
-        for i in range(0, img.width):
-            for j in range(0, img.height):
-                if pix[i, j][0] < pix[i, j][1] and pix[i, j][2] < pix[i, j][1]:
-                    green += 1
-
-        percent = float(green)/(img.width*img.height)
-
-        params = {
-                "grass": percent,
-                "erro": self.cloud_score,
-                "date": self.date
-                }
-        return json.dumps(params)
-
+    params = {
+            "grass": percent,
+            "erro": cloud_score,
+            "date": date
+            }
+    return params
 
 if __name__ == "__main__":
-    grass = Grass(lon=sys.argv[1], lat=sys.argv[2])
-    #print grass.grass()
-    try:
-        file = open('green.json','w')   # Trying to create a new file or open one
-        file.write(grass.grass())
-        file.close()
-
-    except:
-        print('Something went wrong! Can\'t tell what?')
-        sys.exit(0) # quit Python
+    app.run(port=5000)
